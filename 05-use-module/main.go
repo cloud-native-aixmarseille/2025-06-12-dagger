4d591142b83a291dcd22e2b9e7782aa02610b05d
Frederic Leger <frederic@webofmars.com>
2025-06-05 00:55:04 +0200

    fix(use-module): correct code

--- 05-use-module/main.go
+++ 05-use-module/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
+	"dagger/test/internal/dagger"
 )
 
 type Test struct{}
 
-// will run all Go tests
 func (m *Test) Hello(ctx context.Context) (string, error) {
-	m = Coucou{}
-	m.Name = "Meetup Cloud Native Aix Marseille"
-	return Meetup.DitCoucou()
+	str := "Meetup Cloud Native Aix-Marseille"
+	return dag.Coucou().DitCoucou(ctx, dagger.CoucouDitCoucouOpts{
+		Name: str,
+	})
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+# dagger
+
+## using a module
+
+```shell
+dagger init --sdk=go --name test
+dagger install ../04-module
+```
